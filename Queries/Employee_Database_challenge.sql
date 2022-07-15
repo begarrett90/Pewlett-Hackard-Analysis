d353b80837cd017e3a3da454ea1ff1ba1b4d7323
begarrett90 <105942622+begarrett90@users.noreply.github.com>
2022-07-15 12:22:22 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -44,6 +44,20 @@ SELECT DISTINCT ON (e.emp_no)
 	de.from_date,
 	de.to_date,
 	ti.title
-INTO mentorship_eligibility
+-- mentorship_eligibility
 FROM employees as e
 JOIN dept_employees as de
+ON de.emp_no = e.emp_no
+JOIN titles as ti
+ON ti.emp_no = e.emp_no
+WHERE de.to_date = '9999-01-01' AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
+
+
+
+
+
+
+
+
